d6c7ab83a81db398a0a281ac3c4a8c2b6c976e64
Arvind Koormapu <arvindkoormapu@Arvinds-MacBook-Pro.local>
2024-10-23 12:53:06 +0530

    added apple pay text file

--- src/components/Events/Pay.js
+++ src/components/Events/Pay.js
@@ -89,7 +89,7 @@ export default function Pay({
       // Handle merchant validation
       session.onvalidatemerchant = (event) => {
         // Replace this with your serverless function or backend call
-        fetch("/validate-merchant", {
+        fetch(`${process.env.REACT_APP_BASE_URL}/validate-merchant`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -476,7 +476,7 @@ export default function Pay({
                     <input
                       type="radio"
                       name="paymentMethod"
-                      value="samsung"
+                      value="applePay"
                       onChange={() => handleApplePay()}
                       className="text-blue-600"
                     />
