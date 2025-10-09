a5b449263198f5681ce799abad85ba41a342eb6b
arvindkoormapu-hg <arvind.koormapu@innotive.io>
2025-10-09 17:31:31 +0530

    updated view-ticket

--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -72,7 +72,6 @@ export default function Ticket({
 
   useEffect(() => {
     setSpinner(true);
-    window.analytics.page();
     document.title = `Ticket - ${title}`;
     const getOrderDetails = async () => {
       const queryParams = new URLSearchParams(location.search);
